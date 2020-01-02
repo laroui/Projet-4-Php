b5cb4e26e98d01ca339d2585852b80099bf0b4f2
Averndenal <amaury.y.verdenal@gmail.com>
2020-01-02 16:53:22 +0100

    div login

--- contenu/login.php
+++ contenu/login.php
@@ -1,24 +1,17 @@
 <section id="login" class="container__Flex">
     <div id="zone__Login">
         <h3>Connectez-vous</h3>
-        <div class="container__flex wrap">
+        <div class="container__Flex wrap">
             <div class="flex__6">
-                <img src="" alt="image de login">
+                <img src="images/avatars/avatar.png" alt="image de login">
             </div>
             <div class="flex__6">
-                <?php if($connexion == false){ ?>
-                <form action="actions/connexion.php" method="post">
+                <form action="actions/connexion.php" method="post" class="form__Login">
                     <input type="text" id="username" name="username" placeholder="Login">
                     <input type="password" id="pass" name="password" minlength="8" placeholder="Mot de passe" required>
                     <input type="submit" value="Connexion">
                 </form>
-                <?php }else{ ?>
-                    <form action="actions/deconnexion.php" method="post">
-                        <input type="submit" value="Déconnexion">
-                    </form>
-                <?php } ?>
             </div>
         </div>
-        
     </div>
 </section>
